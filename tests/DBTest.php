51b21f68e8a05a4cacd976c9c50e1d37f9600867
wcatron <wcatron@catrondevelopment.com>
2016-02-24 10:23:51 -0500

    Reformat code.

--- tests/DBTest.php
+++ tests/DBTest.php
@@ -7,43 +7,43 @@ use wcatron\MongoDBFramework\MDB;
 
 class DBTest extends PHPUnit_Framework_TestCase {
 
-  public function testSaveAndDelete()
-  {
-    $person = new Person("Unit Test", "Mrs.");
+    public function testSaveAndDelete() {
+        $person = new Person("Unit Test", "Mrs.");
 
-    $this->assertEquals("Mrs. Unit Test", $person->getFullName());
+        $this->assertEquals("Mrs. Unit Test", $person->getFullName());
 
-    $result = $person->save();
-    $this->assertTrue(($result != false));
+        $result = $person->save();
+        $this->assertTrue(($result != false));
 
-    $author = new Author("Art Buchwald");
-    $author->save();
+        $author = new Author("Art Buchwald");
+        $author->save();
 
-    $book = new Book();
-    $book->title = "I Think I Don't Remember";
-    $book->author->set($author);
-    $book->save();
+        $book = new Book();
+        $book->title = "I Think I Don't Remember";
+        $book->author->set($author);
+        $book->save();
 
-    $person->books->add($book);
-    $person->save();
+        $person->books->add($book);
+        $person->save();
 
-    $person_id = $person->getID();
+        $person_id = $person->getID();
 
-    $person = Person::getByID($person_id);
-    $books = $person->books->get();
+        $person = Person::getByID($person_id);
+        $books = $person->books->get();
 
-    $this->assertEquals($books[0]->title, $book->title);
+        $this->assertEquals($books[0]->title, $book->title);
 
-    foreach ($books as $book) {
-      $this->assertEquals($book->owner->name, $person->name);
-      $owners = $book->author->get()->owners->get();
-      $this->assertEquals($owners[0]->name, $person->name);
+        foreach ($books as $book) {
+            $this->assertEquals($book->owner->name, $person->name);
+            $owners = $book->author->get()->owners->get();
+            $this->assertEquals($owners[0]->name, $person->name);
+        }
+
+        $person->delete();
+        $book->delete();
+        $author->delete();
     }
 
-    $person->delete();
-    $book->delete();
-    $author->delete();
-  }
-
 }
+
 ?>
--- tests/src/Book.php
+++ tests/src/Book.php
@@ -6,33 +6,33 @@ use wcatron\CommonDBFramework\LinkedObject;
 use wcatron\MongoDBFramework\Document;
 
 class Book extends Document {
-  var $title;
-  /** @var LinkedObject */
-  var $author;
-  var $owner;
+    var $title;
+    /** @var LinkedObject */
+    var $author;
+    var $owner;
 
-  const COLLECTION = "books";
+    const COLLECTION = "books";
 
-  function __construct() {
-    $this->setObjectForKey(Author::class,'author');
-    $modify = function (&$author)
-    {
-      if ($this->owner) {
-        $author->owners->add($this->owner);
-      }
-    };
-    $this->author->setModifier($modify);
-  }
+    function __construct() {
+        $this->setObjectForKey(Author::class, 'author');
+        $modify = function (&$author) {
+            if ($this->owner) {
+                $author->owners->add($this->owner);
+            }
+        };
+        $this->author->setModifier($modify);
+    }
 
-  function toDocument() {
-    $document = parent::toDocument();
-    $document['title'] = $this->title;
-    return $document;
-  }
-  function fromDocument($document) {
-    parent::fromDocument($document);
-    $this->title = $document['title'];
-  }
+    function toDocument() {
+        $document = parent::toDocument();
+        $document['title'] = $this->title;
+        return $document;
+    }
+
+    function fromDocument($document) {
+        parent::fromDocument($document);
+        $this->title = $document['title'];
+    }
 }
 
 ?>
