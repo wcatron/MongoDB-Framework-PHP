73427c269b48540e426c0bffa0b68159f8744ce4
wcatron <wcatron@catrondevelopment.com>
2016-02-24 16:14:08 -0500

    Resetup travis running from local.

--- autoload.php
+++ autoload.php
@@ -4,12 +4,17 @@ use wcatron\MongoDBFramework\MDB;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-if ($_ENV['travis']) {
-    $config = $_ENV;
-    MDB::configure($config);
-} else {
+var_dump($_ENV);
+
+if (file_exists('config.ini')) {
     $config = parse_ini_file('config.ini');
     MDB::configure($config['mdb_config']);
+} else {
+    $config = [
+        "host" => "127.0.0.1",
+        "db" => "mongodb_framework_php_testing"
+    ];
+    MDB::configure($config);
 }
 
 
